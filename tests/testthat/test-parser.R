b9343037f0286c85bf77224469db1a4caeb59194
Zach Mayer <zach.mayer@gmail.com>
2015-03-12 12:21:11 -0400

    fixed test data path

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -1,6 +1,6 @@
 
 test_that("We can parse the Kaggle sample bracket", {
-  f <- system.file('inst/kaggle_data/sample_submission.csv', package = "kaggleNCAA", mustWork=TRUE)
+  f <- system.file('kaggle_data/sample_submission.csv', package = "kaggleNCAA", mustWork=TRUE)
   dat <- parseBracket(f)
   expect_is(dat, "data.table")
   expect_equal(nrow(dat), 9112)
