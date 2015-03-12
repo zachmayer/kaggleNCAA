81f0859289590334f750fe904672af8147059925
Zach Mayer <zach.mayer@gmail.com>
2015-03-12 12:19:50 -0400

    remove an extra print line

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -1,7 +1,6 @@
 
 test_that("We can parse the Kaggle sample bracket", {
   f <- system.file('inst/kaggle_data/sample_submission.csv', package = "kaggleNCAA", mustWork=TRUE)
-  print(f)
   dat <- parseBracket(f)
   expect_is(dat, "data.table")
   expect_equal(nrow(dat), 9112)
