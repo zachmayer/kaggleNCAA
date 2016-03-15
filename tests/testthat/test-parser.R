b0bfd2fc26a1beae5834d7d94946ce240f9e9475
Zach <zach.mayer@gmail.com>
2016-03-14 21:06:22 -0400

    fix test

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -2,7 +2,6 @@
 test_that("We can parse the Kaggle sample bracket", {
   f <- system.file('kaggle_data/SampleSubmission.csv', package = "kaggleNCAA", mustWork=TRUE)
   dat <- parseBracket(f)
-  dat <- dat[season==2015,]
   expect_is(dat, "data.table")
   expect_equal(nrow(dat), 2278)
   expect_true(dat[,all(team_1 < team_2)])
